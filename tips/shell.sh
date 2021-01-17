3e6992d78d12ab38c16e0b778b32216b9e11e70b
mat <haidar.kabibo@gmail.com>
2021-01-17 05:45:23 +0200

    new

--- tips/shell.sh
+++ tips/shell.sh
@@ -1,3 +1,13 @@
 1- to make "tab" available in reverse shells 
    first make the process in background 
    in your original terminal use (   stty - change and print terminal line settings ) stty raw -echo
+
+
+
+
+2- to make powershell do up and down scrolling 
+   script nameoffile 
+   rlwrap yourlistnercommand(lwarp nc -lvp 9999)
+
+
+
--- windows Privs/priv_esc.sh
+++ windows Privs/priv_esc.sh
@@ -7,7 +7,6 @@ sherlock is deprceted so use Watson exe file, you must built it first
 use scripts inside empire to exploit priv_esc vulun (empire/data/modue/priv_es)
 
 
-use winpeas , upload winpeas.exe that found in /opt/privilege-escalation-awesome-scripts-suite/winPEAS/winPEASexe/winPEAS/bin/x64/Release
-and run it 
+
 
 
